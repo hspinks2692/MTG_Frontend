bec28db3f87c2572a2201754ff9681cdd88aa882
hspinks2692 <hspinks2692@yahoo.com>
2022-10-14 10:29:20 -0400

    Finally added to the Footer file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,13 @@
 function Footer(props) {
-    return <h1>Footer</h1>;
+  const titleStyle = {
+    textAlign: "center",
+    fontSize: "10px",
+    marginLeft: "4%",
+    marginRight: "4%",
+    backgroundColor: "gray"
+  }
+
+    return <p style={titleStyle}>The Gathering was created by Harvey Spinks using the Scryfall API<br/>Done as a personal project with no affiliation<br/>Harvey Spinks 2022</p>
   }
   
   export default Footer;
